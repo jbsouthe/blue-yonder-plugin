4ba8d1c6de07da16277fdb9148ca85830958fe9d
John Southerland <john.southerland@appdynamics.com>
2025-08-05 11:07:30 -0600

    first commit

---
+++ src/main/java-templates/com/cisco/josouthe/MetaData.java
@@ -0,0 +1,7 @@
+package com.cisco.josouthe;
+
+public class MetaData {
+    public static final String VERSION = "v${plugin-version}";
+    public static final String BUILDTIMESTAMP = "${timestamp}";
+    public static final String SUPPORT_URL = "https://github.com/jbsouthe/blue-yonder-plugin";
+}
